0db0d961b0244d0d59c77ec34c5e2c30369a6f4d
Thomas P. Harte <tharte@cantab.net>
2020-11-19 13:18:01 -0500

    make sure it's a deque

--- parsearg/tests/test_data_structures.py
+++ parsearg/tests/test_data_structures.py
@@ -2,6 +2,8 @@ import pytest
 from parsearg.data_structures import Fifo
 
 def test_Fifo():
+    import collections 
+    assert isinstance(Fifo().values, collections.deque)
     assert len(Fifo().values)==0
 
 
