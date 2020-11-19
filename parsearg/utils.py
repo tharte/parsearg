181299a33bc949e29e8a50f5a7104ac3142f7478
Thomas P. Harte <tharte@cantab.net>
2020-11-19 15:03:34 -0500

    superfluous

--- parsearg/utils.py
+++ parsearg/utils.py
@@ -1,8 +1,3 @@
-import sys
-import argparse
-from unittest.mock import Mock
-from collections import deque
-
 def print_list(x, end='\n'):
     assert isinstance(x, (list, set, map, filter, zip))
     for e in x:
