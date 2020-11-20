47d92fe55fbd8a6016aaed025ade158fee700e2a
Thomas P. Harte <tharte@cantab.net>
2020-11-20 15:23:10 -0500

    add test_underline

--- parsearg/tests/test_utils.py
+++ parsearg/tests/test_utils.py
@@ -1,10 +1,16 @@
 import pytest
 from parsearg.utils import (
     is_list_of,
+    underline,
 )
 
 def test_is_list_of():
     assert is_list_of([ 'A', 'B', 'C' ], str)
     assert is_list_of([ (None, []) ], tuple)
     assert is_list_of([ [] ], list)
+
+def test_underline():
+    s = 'hello, world!'
+
+    assert underline(s) == 'hello, world!:\n--------------'
 
