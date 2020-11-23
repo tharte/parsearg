034b0fd2dad92dc82a1cca0f72b3233a7aa39f6d
Thomas P. Harte <tharte@cantab.net>
2020-11-22 22:51:25 -0500

    add User

--- examples/model.py
+++ examples/model.py
@@ -118,3 +118,93 @@ class Todo:
             for record in fetched
         ]
 
+
+class User:
+    tablename = "User"
+
+    def __init__(self, db_filename=db_filename): 
+        self._db_filename = db_filename
+        assert os.path.isfile(self._db_filename)
+
+        try:
+            self._conn = sqlite3.connect(db_filename)
+        except Exception as e:
+            print(e)
+
+    def __del__(self):
+        self._conn.commit()
+        self._conn.close()
+
+    def create(
+        self,
+        name,
+        email=None,
+        phone=None,
+        created=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    ):
+        email = '' if email is None else email
+        phone = '' if phone is None else phone
+
+        sql = (
+            f'insert into {User.tablename} '
+            f'(name, email, phone, created) values '
+            f'("{name}", "{email}", "{phone}", "{created}");'
+        )
+
+        result = self._conn.execute(sql)
+        self._conn.commit()
+
+        return result 
+
+    def select(self, *args):
+        assert all(map(lambda arg: isinstance(arg, str), args))
+        fields = ', '.join(args)
+
+        query  = f'select {fields} from {User.tablename} order by _id;'
+        result = self._conn.execute(query)
+
+        return result.fetchall()
+
+    def delete(self, name):
+        query  = f'delete from {User.tablename} where name="{name}";'
+        self._conn.execute(query)
+        self._conn.commit()
+
+    def purge(self):
+        query  = f'delete from {User.tablename};'
+        self._conn.execute(query)
+
+    def update_email(self, name, email=None):
+        email = '' if email is None else email
+
+        sql = (
+            f'update {User.tablename} '
+            f'set email="{email}" '
+            f'where name="{name}";'
+        )
+
+        self._conn.execute(sql)
+        self._conn.commit()
+
+    def update_phone(self, name, phone=None):
+        phone = '' if phone is None else phone
+
+        sql = (
+            f'update {User.tablename} '
+            f'set phone="{phone}" '
+            f'where name="{name}";'
+        )
+
+        self._conn.execute(sql)
+        self._conn.commit()
+
+    def show(self):
+        query  = f'select * from {User.tablename} order by name;'
+        result  = self._conn.execute(query)
+        fetched = result.fetchall()
+        colnames = [rec[0] for rec in result.description]
+
+        return [
+            {col: record[i] for i, col in enumerate(colnames)}
+            for record in fetched
+        ]
