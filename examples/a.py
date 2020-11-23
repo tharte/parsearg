e2f0a20523dabbf4c9aafe7e7ce84ee839dd7f3b
Thomas P. Harte <tharte@cantab.net>
2020-11-22 23:33:56 -0500

    add a_dict in conftest; add examples a.py and a_aa.py

---
+++ examples/a.py
@@ -0,0 +1,50 @@
+from parsearg.parser import (
+    ParseArg,
+)
+from parsearg.tests.conftest import (
+    a_dict,
+)
+from parsearg.utils import (
+    underline,
+)
+
+def main():
+    # create the parser from the dict (flat tree)
+    parser = ParseArg(d=a_dict(), root_name='root')
+
+    def do_it(node):
+        def _do_it(args):
+            # generate the argparse.Namespace by parsing the arguments:
+            ns     = parser.parse_args(args)
+
+            # perform the associated callback:
+            result = ns.callback(ns)
+
+            # display the result:
+            print('{}\n{}'.format(underline(f'{args!r}'), result))
+
+        print(underline(f'\n\nNODE :: {node!r}'))
+        try:
+            _do_it(f'{node} -h')
+        except SystemExit as e:
+            pass
+        print('\n')
+
+        _do_it(f'{node}')
+        _do_it(f'{node} -v')
+        _do_it(f'{node} -c')
+        _do_it(f'{node} -v -c')
+
+    nodes = [
+        'A',
+        'A B',
+        'A BB',
+        'A BB C',
+        'A BB CC',
+        'A BB CCC',
+    ]
+    list(map(lambda node: do_it(node), nodes))
+
+
+if __name__ == '__main__':
+    main()
--- parsearg/tests/conftest.py
+++ parsearg/tests/conftest.py
@@ -58,6 +58,45 @@ def a_aa_tree():
         ]),
     ])
 
+def a_dict():
+    return {
+        'A': {
+            'callback':     make_callback('A'),
+            '-c':           {'help': 'A [optional pi]', 'action': 'store_const', 'const': 3.141593},
+            '-v|--verbose': {'help': 'A verbosity', 'action': 'store_true'},
+        },
+        'A|B': {
+            'callback':     make_callback('A_B'),
+            '-c':           {'help': 'A B [optional pi]', 'action': 'store_const', 'const': 3.141593},
+            '-v|--verbose': {'help': 'A B verbosity', 'action': 'store_true'},
+        },
+        'A|BB': {
+            'callback':     make_callback('A_BB'),
+            '-c':           {'help': 'A BB [optional pi]', 'action': 'store_const', 'const': 3.141593},
+            '-v|--verbose': {'help': 'A BB erbosity', 'action': 'store_true'},
+            },
+        'A|BB|C': {
+            'callback':     make_callback('A_BB_C'),
+            '-c':           {'help': 'A BB C [optional pi]', 'action': 'store_const', 'const': 3.141593},
+            '-v|--verbose': {'help': 'A BB C verbosity', 'action': 'store_true'},
+        },
+        'A|BB|CC': {
+            'callback':     make_callback('A_BB_CC'),
+            '-c':           {'help': 'A BB CC [optional pi]', 'action': 'store_const', 'const': 3.141593},
+            '-v|--verbose': {'help': 'A BB CC verbosity', 'action': 'store_true'},
+        },
+        'A|BB|CCC': {
+            'callback':     make_callback('A_BB_CCC'),
+            '-c':           {'help': 'A BB CCC [optional pi]', 'action': 'store_const', 'const': 3.141593},
+            '-v|--verbose': {'help': 'A BB CCC verbosity', 'action': 'store_true'},
+        },
+        'A|BBB': {
+            'callback':     make_callback('A_BBB'),
+            '-c':           {'help': 'A BBB [optional pi]', 'action': 'store_const', 'const': 3.141593},
+            '-v|--verbose': {'help': 'A BBB verbosity', 'action': 'store_true'},
+        },
+    }
+
 def a_aa_dict():
     return {
         'A': {
