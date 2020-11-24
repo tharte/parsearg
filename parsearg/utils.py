ecfc67c46133efce46b5a6b07c54be66fffd42c1
Thomas P. Harte <tharte@cantab.net>
2020-11-23 21:35:32 -0500

    add is_argument_field

--- parsearg/utils.py
+++ parsearg/utils.py
@@ -1,3 +1,5 @@
+from types import FunctionType
+
 def print_list(x, end='\n'):
     assert isinstance(x, (list, set, map, filter, zip))
     for e in x:
@@ -13,4 +15,28 @@ def underline(s, underline_char='-'):
     line = underline_char * (len(s)+1)
 
     return f'{s}:\n{line}'
+
+def is_argument_field(field):
+    # Is field a legitimate ArgumentParser.add_argument argument?
+    #
+    # Source:
+    #     https://python.readthedocs.io/en/stable/library/argparse.html#the-add-argument-method
+
+    assert isinstance(field, str)
+
+    fields = [
+        # [str]: name or flags - Either a name or a list of option strings, e.g. foo or -f, --foo.
+        'action',    #  - The basic type of action to be taken when this argument is encountered at the command line.
+        'nargs',     #  - The number of command-line arguments that should be consumed.
+        'const',     #  - A constant value required by some action and nargs selections.
+        'default',   #  - The value produced if the argument is absent from the command line.
+        'type',      #  - The type to which the command-line argument should be converted.
+        'choices',   #  - A container of the allowable values for the argument.
+        'required',  #  - Whether or not the command-line option may be omitted (optionals only).
+        'help',      #  - A brief description of what the argument does.
+        'metavar',   #  - A name for the argument in usage messages.
+        'dest',      #  - The name of the attribute to be added to the object returned by parse_args().
+    ]
+
+    return field in fields 
 
