855d4e62652c0f999d86db1abf5412f7c7684ed9
Thomas P. Harte <tharte@cantab.net>
2023-08-22 15:24:58 +0000

    more formatting/comments

--- run-tests.sh
+++ run-tests.sh
@@ -2,31 +2,32 @@
 
 # env:parsearg
 #
-# the root folder of the package, e.g. the top level of the package structure 
-# as this script lives at the root level, it is just the pwd
-
-# bash$ tree -d $parsearg --charset=ascii | grep -v __pycache 
+#     the root folder of the package, e.g. the top level of the package structure 
+#     as this script lives at the root level, it is just the pwd
 #
-#    /home/tharte/dot/py/python/parsearg
-#    |-- build
-#    |-- dist
-#    |-- doc
-#    |   |-- examples
-#    |   |-- img
-#    |-- install
-#    |   |-- bin
-#    |   |-- include
-#    |   |-- lib
-#    |-- src
-#    |   |-- parsearg
-#    `-- tests
+#     bash$ tree -d $parsearg --charset=ascii | grep -v __pycache 
+#
+#        /home/tharte/dot/py/python/parsearg
+#        |-- build
+#        |-- dist
+#        |-- doc
+#        |   |-- examples
+#        |   |-- img
+#        |-- install
+#        |   |-- bin
+#        |   |-- include
+#        |   |-- lib
+#        |-- src
+#        |   |-- parsearg
+#        `-- tests
 # parsearg=/home/tharte/dot/py/python/parsearg
-
 parsearg=$(pwd)
 
+# env:PYTHONPATH
+#
 # explicitly declare the location of the source code: if the source changes
-# then pytest can pick up the change; otherwise pytest will use the /installed/
-# version of parsearg
+# then pytest can pick up the change; otherwise pytest may use an /installed/
+# version of parsearg (which will not reflect the change in source code)
 PYTHONPATH=$parsearg/src
 
 pytest -v $parsearg/tests
